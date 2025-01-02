77a51e148d5ef57d14579664b38d69e22a1f6a75
Najeeb Sulaiman <beejan003@gmail.com>
2025-01-02 17:04:42 +0000

    refactored code

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -1,71 +1,101 @@
 # Full Code Implementation for the Q&A app below:
 
 import os
+import time
 import pandas as pd
-from sentence_transformers import SentenceTransformer
-#from langchain.vectorstores import Pinecone
+import logging
 from langchain_pinecone import PineconeVectorStore 
 from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFaceHub
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.llms import HuggingFaceEndpoint
+from langchain.chat_models import ChatOpenAI
 from pinecone import ServerlessSpec, Pinecone
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-import time
 from dotenv import load_dotenv
 load_dotenv()
 
 
 # Load and Process Data
 def load_cleaned_data(csv_path):
-    """Load cleaned WhatsApp data from a CSV file."""
-    data = pd.read_csv(csv_path)
-    data['content'] = data['sender'].astype(str) + ': ' + data['message'].astype(str)
-    return data['content'].tolist()[4:6]
+    """
+    Load cleaned WhatsApp data from a CSV file.
+    
+    Parameters:
+    csv_path (str): Path to the csv for the cleaned whatsapp data.
 
-# Generate Embeddings and Store in Vector Database
+    Returns:
+    The concatenated list of senders and messages.
+    """
+    try:
+        data = pd.read_csv(csv_path)
+        data['content'] = data['sender'].astype(str) + ': ' + data['message'].astype(str)
+        logging.info("Data loaded successfully")
+        return data['content'].tolist()
+    except Exception as e:
+        logging.error(f"Failed to load data from {csv_path}: {str(e)}")
+        raise
+
+# Generating Embeddings and Store in Vector Database.
 def create_vector_store(messages, api_key, index_name):
-    """Create a vector database from the messages."""
-    # configure client  
+    """
+    Create a vector database from the messages.
+    
+    Parameters:
+    messages (list): List of WhatsApp messages.
+    api_key (str): Pinecone API KEY. can be gotten from app.pinecone.io.
+    index_name (str): Name of the Pinecone Index to be created, if not exist.
+
+    Returns:
+    A langchain vectore store object of the generated index.
+    """
+    # configuring client.
     pc = Pinecone(api_key=api_key)  
 
     spec = ServerlessSpec(cloud='aws', region='us-east-1')  
-    
-    # check for and delete index if already exists 
-    # if index_name in pc.list_indexes().names():  
-    #     pc.delete_index(index_name)
 
     index = pc.Index(index_name)
         
-    # Generate embeddings
+    # Creating embedding model object.
     model = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 
-    # Check if the index exists, if not, create it
+    # Checking if the index exists, if not, create it
     if index_name not in pc.list_indexes().names():
         pc.create_index(
             index_name, 
             dimension=384, # Dimension for SentenceTransformer embeddings
-            metric='cosine',
+            metric='cosine', # Vectors similarity search metric
             spec=spec)
     
-        # wait for index to be initialized  
+        # waiting for index to be initialized  
         while not pc.describe_index(index_name).status['ready']:  
-            time.sleep(1)  
+            time.sleep(1)
+        
+        logging.info("Index created successfully")
 
-        # Add data to the vector store
+        # Adding data to the vector store.
         vector_store = PineconeVectorStore(index, embedding=model, text_key="sentence-transformer")
         for i, embedding in enumerate(messages):
             vector_store.add_texts([messages[i]], metadatas=[{"id": i}])
 
     else:
+        # If index already exist, use it to create a langchain vectore store object.
         vector_store = PineconeVectorStore(index, embedding=model, text_key="sentence-transformer")
 
     return vector_store
 
 # RAG Workflow Implementation
 def create_rag_chain(vector_store, openai_api_key):
-    """Create a Retrieval-Augmented Generation chain using LangChain."""
+    """
+    Create a Retrieval-Augmented Generation chain using LangChain.
+
+    Parameters:
+    vector_store (obj): Landchain vectore store object.
+    openai_api_key (str): OpenAI API KEY. can be gotten from platform.openai.com.
+    
+    Returns:
+    RAG object chaining the created vector index with LLM
+    """
 
     # chatbot language model
     llm = ChatOpenAI(
@@ -84,7 +114,15 @@ def create_rag_chain(vector_store, openai_api_key):
 
 # Streamlit Web App
 def chatbot_ui(qa_chain):
-    """Build a simple Streamlit UI for the chatbot."""
+    """
+    Build a simple Streamlit UI for the chatbot.
+
+    Parameters:
+    qa_chain (obj): Object chaining the created vector index with LLM.
+    
+    Performs:
+    Creates a simple Q&A ui
+    """
     st.title("Data Engineering Community Chatbot")
 
     user_question = st.text_input("Ask me anything:")
@@ -96,14 +134,14 @@ def chatbot_ui(qa_chain):
 def main():
     """Main function to execute the project pipeline."""
     # Load data
-    data_path = "messages.csv"  # Replace with your CSV path
+    data_path = os.getenv("CSV_PATH")
     messages = load_cleaned_data(data_path)
 
-    # API Keys (Replace with actual keys or use environment variables)
+    # API Keys
     pinecone_api_key = os.getenv("PINECONE_API_KEY")
     openai_api_key=os.getenv("OPENAI_API_KEY")
 
-    index_name = "dec-chat-index"
+    index_name = "dec-chat"
 
     # Vector Store
     vector_store = create_vector_store(messages, pinecone_api_key, index_name)
